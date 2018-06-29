06c640a78244bff0adc6a66a7f9fb5fa77bfa8e3
Alex Cubero <alexcuar@gmail.com>
2018-06-29 20:41:01 +0200

    Tercer lección. Definicion de variables

--- leccion1/variables-basicas.go
+++ leccion1/variables-basicas.go
@@ -9,7 +9,7 @@ func main() {
 	var suma int = 8 + 9
 	var resta int = 6 - 4
 	var nombre string = "ACubero"
-	fmt.Println("Hola mundo por " + nombre)
+	fmt.Println("Hola mundo por  " + nombre)
 	fmt.Println(suma)
 	fmt.Println(resta)
 
