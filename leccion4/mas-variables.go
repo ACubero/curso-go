db1baf9444b29a61e581ca901887294b57ac39ea
Alex Cubero <alexcuar@gmail.com>
2018-07-03 22:19:12 +0200

    Leccion 4

---
+++ leccion4/mas-variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var suma int = 8 + 9
+	var resta int = 6 - 4
+	var nombre string = "Alex"
+
+	var apellidos string = "Cubero ALCUAR"
+	apellidos = "Tomás Andresa"
+
+	pais := "España"
+	pais = "Colombia"
+
+	fmt.Println("Hola mundo desde Go con " + nombre + " " + apellidos + " " + pais)
+	fmt.Println(suma)
+	fmt.Println(resta)
+}
