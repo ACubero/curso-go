b3b8795560348bc5f9c40b1fbe5065957dc12bd5
Alex Cubero <alexcuar@gmail.com>
2018-07-05 21:30:23 +0200

    Leccion6. Operadores aritméticos

---
+++ leccion6/operadores-aritmeticos.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero1 int = 10
+	var numero2 int = 2
+
+	//Suma
+	fmt.Println("Sumar: ")
+	fmt.Println(numero1 + numero2)
+
+	//Resta
+	fmt.Println("Restar:")
+	fmt.Println(numero1 - numero2)
+
+	//Multiplacion
+	fmt.Println("Multiplicación:")
+	fmt.Println(numero1 * numero2)
+
+	//División
+	fmt.Println("División:")
+	fmt.Println(numero1 / numero2)
+
+	//Resto
+	fmt.Println("Resto:")
+	fmt.Println(numero1 % numero2)
+}
