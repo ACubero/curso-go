8943493742bad1eb0f65c85bed6b5ecf8c1fc00b
Alex Cubero <alexcuar@gmail.com>
2018-07-08 18:11:44 +0200

    Leccion 9. Closures: funciones dentro de funciones

---
+++ leccion9/closures.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(saberPrecioPedido(18, 89.2))
+}
+func saberPrecioPedido(numArticulos float32, preciounitario float32) (string, float32) {
+	// Esto que definimos a continuación es el closures
+	precio := func() float32 {
+		return numArticulos * preciounitario
+	}
+
+	return "El precio del pedido es:", precio()
+}
