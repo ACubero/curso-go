8082cea1b4a6429e6820593e56c5a510c04c9b72
ACubero <alexcuar@gmail.com>
2018-07-14 22:06:55 +0200

    Leccion 11. Arrays. Dos maneras de definirlos

---
+++ leccion11/arrays.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Definición de array forma 1
+	var vtestamento [3]string
+	vtestamento[0] = "Génesis"
+	vtestamento[1] = "Éxodo"
+	vtestamento[2] = "Levítico"
+
+	fmt.Println(vtestamento)
+
+	//Definición de arrays forma 2
+	ntestamento := [3]string{"Mateo", "Marcos", "Lucas"}
+
+	fmt.Println(ntestamento)
+}
