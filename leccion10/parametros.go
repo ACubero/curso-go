74c674676e89c71759087dbe2633ef7178d8ea6a
Alex Cubero <alexcuar@gmail.com>
2018-07-14 22:14:06 +0200

    Leccion 10

--- leccion10/parametros.go
+++ leccion10/parametros.go
@@ -12,7 +12,7 @@ func main() {
 func paramIlimitados(parametros ...string) {
 
 	//El for necesita una variable, cuando ponemos el subrallado bajo _ sustituimos esa variable
-	// Este for lo que hace es recorrer todo lo que hay guardado en parámetros y cada uno de los
+	// Este for lo que hace es recorrer todo  lo que hay guardado en parámetros y cada uno de los
 	// contenidos que tiene se pasa a la variable parametros
 	for _, parametro := range parametros {
 		fmt.Println(parametro)
