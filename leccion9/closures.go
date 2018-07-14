3bcac2cbbb148c0877c0e3faa4a964ae2051cadf
Alex Cubero <alexcuar@gmail.com>
2018-07-14 22:13:49 +0200

    Leccion 9

--- leccion9/closures.go
+++ leccion9/closures.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Println(saberPrecioPedido(18, 89.2))
 }
 func saberPrecioPedido(numArticulos float32, preciounitario float32) (string, float32) {
-	// Esto que definimos a continuación es el closures
+	// Esto que definimos a continuación es  el closures
 	precio := func() float32 {
 		return numArticulos * preciounitario
 	}
