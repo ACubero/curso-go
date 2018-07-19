a0f5c6acc647553e6303e6ec3aa19eee5bb2e4e8
Alex Cubero <alexcuar@gmail.com>
2018-07-19 20:28:30 +0200

    Leccion 14. Slices. Arrays sin necesidad de indicar el número de registros que tendrá. Dinámico

---
+++ leccion14/slices.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Un slice es un array pero sin definir el número de registros fijos que habrá
+	ejemploSlice := []string{
+		"primero",
+		"segundo",
+		"tercero",
+		"cuarto",
+		"quinto"}
+
+	fmt.Println(ejemploSlice)
+
+}
