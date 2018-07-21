22ea88640934fee4373b939ae321cc8d941c649b
Alex Cubero <alexcuar@gmail.com>
2018-07-21 12:56:32 +0200

    ..

---
+++ leccion17/argumentos.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("** Conversor de medidas ***")
+
+	fmt.Println("Introduce convertir " + os.Args[1] + " a " + os.Args[2])
+
+}
