22774a2b86b8a163b7109fa22af7d943216f985d
Alex Cubero <alexcuar@gmail.com>
2018-07-21 22:40:38 +0200

    Leccion 17. Argumentos

--- leccion17/argumentos.go
+++ leccion17/argumentos.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("** Conversor de medidas ***")
-
+	// Recogemos el parámetro1 y parametro 2
 	fmt.Println("Introduce convertir " + os.Args[1] + " a " + os.Args[2])
 
 }
