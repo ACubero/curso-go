f3aee5f690a4523d583d9c525e5d0577bd9788b1
ACubero <alexcuar@gmail.com>
2018-11-24 12:19:44 +0100

    API-REST. Añadir la libreria de ROUTER -- GORILLA/MUX

--- api-rest/main.go
+++ api-rest/main.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hola mundo desde mi servidor web con GO")
-	})
+	router := mux.NewRouter().StrictSlash(true) // URLS amigables
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contacto", Contact)
 
-	server := http.ListenAndServe(":8080", nil)
+	server := http.ListenAndServe(":8080", router)
 
 	log.Fatal(server)
+}
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola mundo desde mi servidor web con GO")
+}
+func Contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Estamos en la página de contactos")
 }
